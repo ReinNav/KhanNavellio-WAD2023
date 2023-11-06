fba705d558610cca46482ec3552e30aab9e9dbe8
Reinardus Navellio <reintirta@gmail.com>
2023-11-06 21:09:35 +0100

    change footer colour, added leaflet for point popups

--- front-end/map.js
+++ front-end/map.js
@@ -2,15 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
     L.mapquest.key = 'BTjR7udbEih1QrCTjZUq7w1m25Eket6l';
 
     var map = L.mapquest.map('map', {
-        center: [52.5573155, 13.3729101], // Default center coordinates (Berlin)
+        center: [52.5573155, 13.3729101], // coordinates on load (Berlin)
         layers: L.mapquest.tileLayer('map'),
-        zoom: 9 // Default zoom level
+        zoom: 9 
     });
 
-    // Example marker
-    var marker = L.marker([52.5573155 , 13.3729101]).addTo(map);
-    var marker1 = L.marker([52.38300635 , 12.610102564424093]).addTo(map);
-    var marker2 = L.marker([52.3928, 13.7892]).addTo(map);
+    var marker1 = L.marker([52.5573155 , 13.3729101]).addTo(map);
+    marker1.bindPopup('Kiez in der Schwedenstraße');
+    
+    var marker2 = L.marker([52.38300635 , 12.610102564424093]).addTo(map);
+    marker2.bindPopup('Zigarettenfabrik');
 
+    var marker3 = L.marker([52.3928, 13.7892]).addTo(map);
+    marker3.bindPopup('Tesla Giga Factory');
+
+    marker1.on('click', function(e) {
+        marker1.openPopup();
+    });
+    marker2.on('click', function(e) {
+        marker2.openPopup();
+    });
+    marker3.on('click', function(e) {
+        marker3.openPopup();
+    });
    
 });
