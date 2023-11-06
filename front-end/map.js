d43bc6c514bd0687952b4be29648c427474b5f2e
illusionSUB07 <daniyalkhan@Yusufzais-MacBook-Pro.local>
2023-11-06 20:23:48 +0100

    Some changes made

--- front-end/map.js
+++ front-end/map.js
@@ -2,11 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     L.mapquest.key = 'BTjR7udbEih1QrCTjZUq7w1m25Eket6l';
 
     var map = L.mapquest.map('map', {
-        center: [52.5200, 13.4050], // Default center coordinates (Berlin)
+        center: [52.5573155, 13.3729101], // Default center coordinates (Berlin)
         layers: L.mapquest.tileLayer('map'),
-        zoom: 12 // Default zoom level
+        zoom: 9 // Default zoom level
     });
 
     // Example marker
-    var marker = L.marker([52.5200, 13.4050]).addTo(map);
+    var marker = L.marker([52.5573155 , 13.3729101]).addTo(map);
+    var marker1 = L.marker([52.38300635 , 12.610102564424093]).addTo(map);
+    var marker2 = L.marker([52.3928, 13.7892]).addTo(map);
+
+   
 });
